4857fe2535de8376808df2e93758a386f01fdc6a
Archit Dey <deyboy90@gmail.com>
2025-04-20 20:49:31 -0700

    Fix alignments

--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,9 @@ import CertificationsSection from "@/components/certifications-section"
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen items-center">
       <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-        <div className="container flex h-16 items-center justify-between">
+        <div className="container mx-12 flex h-16 items-center justify-between">
           <Link href="/" className="font-bold text-xl">
             Archit Dey
           </Link>
