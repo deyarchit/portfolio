f39f07ddc5d14dda925c33fe42b13e7fa01bde88
Archit Dey <deyboy90@gmail.com>
2025-04-21 14:51:06 -0700

    Update image path

--- app/page.tsx
+++ app/page.tsx
@@ -103,7 +103,7 @@ export default function Home() {
               <div className="flex items-center justify-center">
                 <div className="w-64 h-64 rounded-full overflow-hidden">
                   <Image
-                    src="/Archit.jpeg"
+                    src="./Archit.jpeg"
                     alt="Archit Dey"
                     className="w-full h-full object-cover"
                     unoptimized
