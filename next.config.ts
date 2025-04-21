a6b2b3047dce340eca380e6b06c8dc3c607d4768
Archit Dey <deyboy90@gmail.com>
2025-04-21 12:06:24 -0700

    Disable lint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors. 
+    // https://nextjs.org/docs/app/api-reference/config/next-config-js/eslint
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
